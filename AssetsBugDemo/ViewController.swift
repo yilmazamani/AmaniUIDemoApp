11445aac03a6d260e4c31e3adaed05b3ff5fac55
yilmazamani <yilmaz@amani.tech>
2025-01-14 17:12:30 +0300

    feat: create demo app for amaniui

--- AssetsBugDemo/ViewController.swift
+++ AssetsBugDemo/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import AmaniUI
+import AmaniSDK
 
 class ViewController: UIViewController {
+  let amaniUI = AmaniUI.sharedInstance
 
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    amaniUI.setDelegate(delegate: self)
+    
+    amaniUI.set(server: "SERVER", token:"TOKEN", language: "tr", apiVersion: .v2)
+    
+    amaniUI.showSDK(on: self) {[weak self](customer, error) in
+      if let error = error {
+        fatalError(error.localizedDescription)
+      } else {
+        self?.amaniUI.showSDK(on: self!) {[weak self](customer, error) in
+          if let error = error {
+            fatalError(error.localizedDescription)
+          } else {
+            print(customer)
+          }
+        }
+      }
+    }
   }
-
-
+  
 }
-
+extension ViewController: AmaniUIDelegate{
+  
+  func onError(type: String, Error: [AmaniSDK.AmaniError]) {
+    print(type, Error)
+  }
+  
+  func onKYCSuccess(CustomerId: String) {
+    print("onKYCSuccess", CustomerId)
+  }
+  
+  func onKYCFailed(CustomerId: String, Rules: [[String : String]]?) {
+    print("onKYCFailed", CustomerId, Rules)
+  }
+}
---
+++ Podfile
@@ -0,0 +1,15 @@
+source "https://github.com/AmaniTechnologiesLtd/Mobile_SDK_Repo"
+source "https://github.com/CocoaPods/Specs"
+
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+use_frameworks! :linkage => :static
+
+target 'AssetsBugDemo' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  #use_frameworks!
+  # Pods for AssetsBugDemo
+
+pod 'AmaniUI'
+
+end
